87b6803277a382c984ed911d2ba4a18b52f9acfa
Andrés Rengifo <andresm.rengifo@gmail.com>
2022-09-29 19:21:02 -0500

    añadi algunas funcionalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { perfilModule } from './features/perfil/perfil.module';
 import { LoginModule } from './features/login/login.module';
-//components
 
+
+//components
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { BarChartComponent } from './features/bar-chart/bar-chart.component';
 import { BarGraphComponent } from './features/bar-graph/bar-graph.component';
 
@@ -13,7 +13,6 @@ import { BarGraphComponent } from './features/bar-graph/bar-graph.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     BarChartComponent,
     BarGraphComponent,
   ],
